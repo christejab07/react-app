68fd25de0276cde3aa09093fd6ff7cb5bd876e5e
Christelle <122173362+christejab07@users.noreply.github.com>
2023-05-03 21:20:34 +0200

    some modifications while studying

--- src/components/pages/services.js
+++ src/components/pages/services.js
@@ -1,6 +1,6 @@
 import React from "react";
 import '../../App.css'
 
-export default function Products(){
+export default function Services(){
      return <h1 className="services">SERVICES</h1>
 }
--- src/components/pages/signUp.js
+++ src/components/pages/signUp.js
@@ -1,6 +1,6 @@
 import React from "react";
 import '../../App.css'
 
-export default function Products(){
+export default function SignUp(){
      return <h1 className="sign-up">SIGN UP</h1>
 }
